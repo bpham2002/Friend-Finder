955e7fd05de9c6eac8e58b837372beba37eb7535
unknown <bpham2002@gmail.com>
2018-04-27 18:45:45 -0700

    updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,23 +5,7 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friendArray = [{
-    Name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-
-}];
+var friendArray = [];
 
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendArray;
